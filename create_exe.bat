8ad573b0be00778371804bf090b83bebc87bdce7
Junko <126285554+Junko666@users.noreply.github.com>
2025-02-11 17:15:59 +0100

    Update create_exe.bat

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,22 @@
 @echo off
+
+echo Installiere pyinstaller...
 pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --icon "autostart_monitor.ico"  "autostart_monitor.py"
+
+if %ERRORLEVEL% NEQ 0 (
+   echo Fehler beim Installieren von pyinstaller.
+   pause
+   exit /b %ERRORLEVEL%
+)
+
+echo Erstelle ausführbare Datei mit pyinstaller...
+python -m pyinstaller --noconfirm --onefile --windowed --icon "autostart_monitor.ico" "autostart_monitor.py"
+
+if %ERRORLEVEL% NEQ 0 (
+   echo Fehler bei der Erstellung der ausführbaren Datei.
+   pause
+   exit /b %ERRORLEVEL%
+)
+
+echo Erstellung abgeschlossen.
+pause
